5f75b5e7ed3263a4b26149d7f44801a6753cacc1
Ian Langworth ☠ <ian@langworth.com>
2010-12-30 01:13:39 -0800

    A little noble.cc cleanup

--- src/noble.cc
+++ src/noble.cc
@@ -1,10 +1,13 @@
-#include <iostream>
+// Copyright 2010 the Noble project authors. All rights reserved.
+
 #include <v8.h>
+
+#include <string>
 
 using namespace std;
 using namespace v8;
 
 int main(int argc, char* argv[]) {
-  cout << "i'm feeling noble!\n";
+  printf("i'm feeling noble!\n");
   return 0;
 }
